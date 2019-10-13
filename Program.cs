15b050177bfd9f765a7c84a3fc385e3906942684
Alexander <alex.esbj@gmail.com>
2019-10-13 22:09:49 +0200

    spooky scarry skeleton done

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace humans_and_animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Day loop
+            int nrOfMeatLeft = 50;
+            int nrOfVeggiesLeft = 50;
+            int day = 0;
+            while (true)
+            {
+                day++;
+                Console.WriteLine("Det är dag " + day + ":");
+                Console.WriteLine("-------");
+
+                // LÄGG TILL DIN NYA KOD HÄR
+
+                // Wait for the next day
+                Console.ReadKey();
+            }
+        }
+    }
+}
